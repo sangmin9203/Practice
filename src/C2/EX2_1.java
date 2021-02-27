88d23a24c73fac8c8739d379ed3a5606838b3fb6
sangmin9203 <ysm9203@gmail.com>
2021-02-27 23:41:28 +0900

    2021_2_28

--- src/C2/EX2_1.java
+++ src/C2/EX2_1.java
@@ -1,14 +1,18 @@
 package C2;
 
-import java.awt.List;
-
 public class EX2_1 {
 	
 	public static void main(String[] args) {
-		int [] a = {0,37,51,0,74};
+		int [] a = new int[5];
 		
-		for(int i =0; i<a.length; i++) {
-			System.out.println("a["+i+"] : "+a[i]);
+		a[0] = 1;
+		a[1] = 2;
+		a[2] = 3;
+		a[3] = 4;
+		a[4] = 5;
+/*		int [] a = new int[5] {1,2,3,4,5}; 로도 표현 가능 */		
+		for(int i = 0; i<a.length; i++) {
+			System.out.println("a["+i+"] = "+a[i]);
 		}
 	}
 }
---
+++ src/C2/EX2_4.java
@@ -0,0 +1,35 @@
+package C2;
+
+import java.util.Scanner;
+
+public class EX2_4 {
+	
+	public static void main(String[] args) {
+		Scanner sc1 = new Scanner(System.in); 
+		System.out.println("키의 최댓값을 구합니다.");
+		System.out.print("사람의 수 : ");
+		int n = sc1.nextInt();
+		int [] a = new int[n];
+		
+		for(int i = 0; i<a.length; i++) {
+			System.out.print("height["+i+"] : ");
+			a[i] = sc1.nextInt();
+		}
+		
+		System.out.println("최댓값은 "+maxOf(a)+"입니다.");
+		
+		
+		
+	}
+	
+	static int maxOf(int [] a) {
+		int max = a[0];
+		for(int i = 0; i<a.length; i++) {
+			if(a[i]>max) {
+				max = a[i];
+			}
+		}
+		return max;
+		
+	}
+}
